09bd92ebc9c8a37b83d3a9e4cdcbd930ade76b93
Santiago <santiiago.toro2011@gmail.com>
2022-02-09 22:43:42 -0500

    Initial commit, first phase of development, pending add bloc pattern

---
+++ lib/components/rounded_password_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/components/text_field_container.dart';
+import 'package:flutter_auth/constants.dart';
+
+class RoundedPasswordField extends StatelessWidget {
+  final String defaultValue;
+  final ValueChanged<String> onChanged;
+  const RoundedPasswordField({
+    Key key,
+    this.defaultValue,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        obscureText: true,
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          hintText: defaultValue,
+          icon: Icon(
+            Icons.lock,
+            color: kPrimaryColor,
+          ),
+          suffixIcon: Icon(
+            Icons.visibility,
+            color: kPrimaryColor,
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
